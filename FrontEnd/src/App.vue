63b807ddd639314b1d1d3c375afa2692d7fa1a51
Yasiru Wickramasinghe <78946418+YasiruWickramasinghe@users.noreply.github.com>
2021-09-30 21:23:42 +0530

    Update Task Tracker

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Task Tracking App"/>
+  <HelloWorld msg="Welcome to Task Tracking App"/>
 </template>
 
 <script>
